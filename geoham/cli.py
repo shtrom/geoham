97aec53c8274b6e3c8fd80f78c46a34d0f2d5fd7
Olivier Mehani <shtrom@ssji.net>
2018-05-20 23:26:18 +1000

    Add downloader
    Signed-off-by: Olivier Mehani <shtrom@ssji.net>

--- geoham/cli.py
+++ geoham/cli.py
@@ -1,5 +1,15 @@
 import click
+import logging
 
-@click.command()
-def main():
+from geoham.downloader import Downloader
+
+@click.group()
+@click.option('--log_level', '-l', default='info')
+def main(log_level):
     click.echo('main')
+    logging.basicConfig(level=log_level.upper())
+
+@main.command()
+def download():
+    downloader = Downloader()
+    downloader.download()
---
+++ geoham/downloader.py
@@ -0,0 +1,28 @@
+import logging
+try:
+    # Python 3
+    from urllib.request import urlretrieve
+except ModuleNotFoundError:
+    # Python 2
+    from urllib import urlretrieve
+
+class Downloader:
+
+    _logger = None
+
+    _url='http://www.wia.org.au/members/repeaters/data/documents/Repeater%20Directory%20180318.csv'
+
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+
+    def download(self):
+        outfile = self._url.split('/')
+        if len(outfile) > 1:
+            outfile = outfile[-1]
+        else:
+            outfile = None
+
+        self._logger.info('Downloading %s...' % self._url)
+        filename, httpmessage  = urlretrieve(self._url, outfile)
+        self._logger.info('Saved as %s' % filename)
+        return filename
