670821d32559c1398189e55a84fe47f85c28c321
Olivier Mehani <shtrom@ssji.net>
2018-05-21 22:46:51 +1000

    Separate setup's install_requires from requirements.txt
    Signed-off-by: Olivier Mehani <shtrom@ssji.net>

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 from distutils.core import setup
 
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
-install_reqs = parse_requirements('requirements.txt', session=False)
-reqs = [str(ir.req) for ir in install_reqs]
-
 setup(name='geoham',
       version='0.0.1pre',
       description="Library to manipulate geographic data about amateur radio repeaters and frequencies",
@@ -16,7 +8,9 @@ setup(name='geoham',
       author_email="shtrom+geoham@ssji.net",
       url="http://scm.narf.ssji.net/git/geoham",
       packages=['geoham'],
-      install_requires=reqs,
+      install_requires=[
+          'Click',
+      ],
       entry_points={
           'console_scripts': [
               'geoham = geoham.cli:main'
