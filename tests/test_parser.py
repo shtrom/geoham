6eaa04572446775d79d429692a1315036b9495d8
Olivier Mehani <shtrom@ssji.net>
2019-02-04 22:22:29 +1100

    Add stub test for parser
    Signed-off-by: Olivier Mehani <shtrom@ssji.net>

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,9 +1,30 @@
 import unittest
 
+try:
+    # for Python 2.x
+    from StringIO import StringIO
+except ImportError:
+    # for Python 3.x
+    from io import StringIO
+import csv
+
+from geoham.parser import Parser
+
 
 class TestParser(unittest.TestCase):
+
+    TEST_CSV = '''Output, Input, Call, mNemonic, Location, Service Area, Latitude, Longitude, S, ERP, HASL, T/O, Sp, Tone, Notes
+29.1200, 29.1200, VK2RMB, THills, Terrey Hills, Sydney 6/15, -33.692994, 151.222079, P, -, 150, -, 2MB, -, 1
+29.1200, 29.1200, VK2RBH, Darlng, Mt Darling, Broken Hill, -32.022963, 141.533162, O, -, 400, -, 2DPG, -, 12'''
+
     def test_parse(self):
-        self.assertTrue(False)
+        f = StringIO(self.TEST_CSV)
+        parser = Parser()
+
+        parsed, skipped = parser.parse(f)
+
+        self.assertIsNotNone(parsed)
+        self.assertIsNone(skipped)
 
 
 if __name__ == '__main__':
